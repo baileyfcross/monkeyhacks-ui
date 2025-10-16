28edc00e89d9e7bcced7b773d60d80de3950fe0c
baileyfcross <bf02cros@siena.edu>
2025-10-16 04:07:37 -0400

    Added spotify widget and consolidated styles and refactored styling

---
+++ src/components/LandmarkErickEmbed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="landmark-erick-embed">
+    <iframe
+      data-testid="embed-iframe"
+      style="border-radius: 12px"
+      src="https://open.spotify.com/embed/artist/5JVDnNOK4mEKtNLGKyzZAv?utm_source=generator"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="eager"
+      volume="0.5"
+    ></iframe>
+  </div>
+</template>
