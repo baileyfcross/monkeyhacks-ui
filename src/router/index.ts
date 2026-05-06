6e0e248afc6bc0fb484381a3fbd410c1306e3fb0
baileyfcross <bf02cros@siena.edu>
2026-05-06 13:53:08 -0400

    added new game for website

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import DiceView from '@/views/DiceView.vue'
 import PlinkoView from '@/views/PlinkoView.vue'
+import AimDuelView from '@/views/AimDuelView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/diceGame', name: 'dice', component: DiceView },
     { path: '/plinko', name: 'plinko', component: PlinkoView },
+    { path: '/aim-duel', name: 'aim-duel', component: AimDuelView },
   ],
 })
 
